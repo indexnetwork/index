5ddf443136b5d0c961bfb47c3ac3de63fc90bee0
cnsndnz <82813495+cnsndnz@users.noreply.github.com>
2022-08-11 00:19:42 +0300

    Landing section 1 fix

--- src/components/site/landing/LandingSection1/index.tsx
+++ src/components/site/landing/LandingSection1/index.tsx
@@ -1,43 +1,39 @@
 import Header from "components/base/Header";
 import Text from "components/base/Text";
-import React, { useState } from "react";
-import Lottie from "react-lottie-player";
+import React from "react";
 import cc from "classcat";
 import Button from "components/base/Button";
 import cm from "./style.module.scss";
-import anim from "./anim.json";
 import LandingSection from "../LandingSection";
 
-const LandingSection1: React.VFC = () => {
-	const [loaded, setLoaded] = useState(false);
-	return (
-		<LandingSection dark>
-			<div
-				className="idx-lnd-card idx-lnd-first"
-			>
-				<div className="idx-lnd-desc">
-					<Header className={cm.blueTitle}>The human bridge between context and content.</Header>
-					<Text className={cc([cm.descLine, cm.mbMd])}>Index.as helps you to curate content and<br />create searchable indexes.</Text>
-					<Button>Create your first index</Button>
-				</div>
-				<div
-					className={loaded ? "idx-lnd-img idx-lnd-img-reverse idx-lnd-lottie idx-lnd-lottie-fade" : "idx-lnd-img idx-lnd-img-reverse idx-lnd-lottie"}
-				>
-					<Lottie
-						loop
-						animationData={anim}
-						play
-						style={{
-							height: "100%",
-							width: "100%",
-						}}
-						onLoad={(e) => {
-							setLoaded(true);
-						}}
-					/>
-				</div>
+const LandingSection1: React.VFC = () => (
+	<LandingSection dark>
+		<div
+			className="idx-lnd-card idx-lnd-first"
+		>
+			<div className="idx-lnd-desc">
+				<Header className={cm.blueTitle}>The human bridge between context and content.</Header>
+				<Text className={cc([cm.descLine, cm.mbMd])}>Index.as helps you to curate content and<br />create searchable indexes.</Text>
+				<Button>Create your first index</Button>
 			</div>
-		</LandingSection>
-	);
-};
+			<div
+				className={"idx-lnd-img idx-lnd-img-reverse idx-lnd-lottie idx-lnd-lottie-fade"}
+			>
+				<video
+					autoPlay
+					loop
+					muted
+					playsInline
+					style={{
+						height: "100%",
+						width: "100%",
+					}}
+				>
+					<source src="video/anim1.webm" type='video/webm; codecs=vp9' />
+				</video>
+			</div>
+		</div>
+	</LandingSection>
+);
+
 export default LandingSection1;
--- src/types/index.ts
+++ src/types/index.ts
@@ -6,9 +6,9 @@ import React, {
 /**
  * Common Types
  */
-export type NextPageWithLayout = NextPage & {
+export type NextPageWithLayout<T = {}> = NextPage<T> & {
   requireAuth?: boolean;
-  getLayout?: (page: ReactElement) => ReactNode
+  getLayout?: (page: ReactElement) => ReactNode;
 };
 
 export type GridFractionType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
