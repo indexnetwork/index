7175e2a4ff08a0567990fe9aab49fb366d1bd8a0
furkanozelge <furkancoc11@gmail.com>
2022-12-03 01:27:26 +0300

    fixed apiurl

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
-	baseUrl: "https://dev.index.as/",
-	apiUrl: "https://dev.index.as/api",
+	baseUrl: "https://testnet.index.as/",
+	apiUrl: "https://testnet.index.as/api",
 	ipfsProxy: "https://ipfs.io/ipfs/",
 	ipfsInfura: "https://ipfs.infura.io:5001/api/v0",
 	ceramicNode: "https://testnet.index.as/ceramic",
