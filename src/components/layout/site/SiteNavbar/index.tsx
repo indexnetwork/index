81505f7a384978b729054b2870baec5acf508aec
serafettin <mail@serafettin.net>
2023-01-23 21:33:27 +0300

    @furkanozelge use button props for loading state

--- src/components/layout/site/SiteNavbar/index.tsx
+++ src/components/layout/site/SiteNavbar/index.tsx
@@ -85,6 +85,7 @@ const SiteNavbar: React.FC<LandingHeaderProps> = ({ headerType = "user", isLandi
 			{...baseProps}
 		>
 			<NavbarMenu placement="right">
+				// TODO use props for loading
 				{loading || (authenticated && isLanding) ? (
 					<Button
 						style={	{ background: "#7F7F7F !important", borderColor: "#7F7F7F !important;" } }
