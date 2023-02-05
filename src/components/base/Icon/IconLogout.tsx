da8e51ce7513254c072c491bf6fec2755c66a5a5
Furkan Özelge <furkancoc11@gmail.com>
2023-02-05 17:41:50 +0300

    icon changes and new tab image include

--- src/components/base/Icon/IconLogout.tsx
+++ src/components/base/Icon/IconLogout.tsx
@@ -1,22 +1,39 @@
 import * as React from "react";
 
-const IconLogout: React.FC<React.SVGProps<SVGSVGElement>> = ({ stroke = "var(--main)", strokeWidth = "1.2", ...props }) => (
-	<svg className="icon"
-		width={16}
-		height={16}
-		viewBox="0 0 16 16"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-		stroke={stroke}
-		strokeWidth={strokeWidth}
-		{...props}
-	>
-		<path
-			d="M10.667 11.333 14 8l-3.333-3.333M14 8H6M6 14H3.333A1.334 1.334 0 0 1 2 12.667V3.333A1.333 1.333 0 0 1 3.333 2H6"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		/>
-	</svg>
+const IconLogout: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  stroke = "var(--main)",
+  strokeWidth = "1.2",
+  ...props
+}) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.333 14.1668L17.4997 10.0002L13.333 5.8335"
+      stroke="#FF233E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M17.5 10H7.5"
+      stroke="#FF233E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M7.5 17.5H4.16667C3.72464 17.5 3.30072 17.3244 2.98816 17.0118C2.67559 16.6993 2.5 16.2754 2.5 15.8333V4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5H7.5"
+      stroke="#FF233E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
 );
 
 export default IconLogout;
