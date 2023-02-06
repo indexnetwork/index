f3ae89e54dbc1867e8e872b98074781a7328c30a
serafettin <mail@serafettin.net>
2023-02-07 00:07:42 +0300

    Connector final

--- datastores/install.sh
+++ datastores/install.sh
@@ -6,7 +6,8 @@ helm upgrade --install redis bitnami/redis --values values-redis.yaml
 kubectl apply -f kafkaconnect.yaml
 
 curl -X DELETE \
-  http://kafkaconnect-service.composedb:8083/connectors/containers-connector
+  http://kafkaconnect-service.composedb:8083/connectors/containers-connector-final-running-3
+
 
 curl -X POST \
   http://kafkaconnect-service.composedb:8083/connectors \
@@ -19,5 +20,5 @@ curl -X PUT \
   -H 'Content-Type: application/json' \
   -d 
 
-  
+
  kubectl run -it --rm debezium-ui --env KAFKA_CONNECT_URIS=http://kafkaconnect-service:8083 --image debezium/debezium-ui:latest
