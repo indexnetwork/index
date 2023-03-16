8c61858b5b815a3c4be2759385eff941956485d9
serafettin <mail@serafettin.net>
2023-03-16 18:05:02 +0300

    iterate

--- ceramic/init.sh
+++
@@ -1,8 +0,0 @@
-rm priv.key
-rm pub.key
-rm -rf generated
-
-composedb did:generate-private-key >> priv.key
-composedb did:from-private-key $(cat priv.key) > pub.key
-
-export DID_PRIVATE_KEY=$(cat priv.key)
