8a72650f777befba546daa67b734671e6e389c7a
serafettin <mail@serafettin.net>
2023-04-15 12:42:19 +0300

    Use same signature for Ceramic and LIT Protocol

--- src/components/site/context/AuthHandlerProvider.tsx
+++ src/components/site/context/AuthHandlerProvider.tsx
@@ -1,10 +1,15 @@
-import { DIDSession } from "did-session";
-import { EthereumWebAuth, getAccountId } from "@didtools/pkh-ethereum";
+import { getAccountId } from "@didtools/pkh-ethereum";
+import { Cacao, SiweMessage } from "@didtools/cacao";
+import { normalizeAccountId } from "@ceramicnetwork/common";
 
 import { useAppDispatch, useAppSelector } from "hooks/store";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import ceramicService from "services/ceramic-service";
+import { getAddress } from "@ethersproject/address";
+import { randomBytes, randomString } from "@stablelib/random";
+import { DIDSession, createDIDKey, createDIDCacao } from "did-session";
+
 import {
 	disconnectApp, selectConnection, setAuthLoading, setCeramicConnected, setMetaMaskConnected,
 } from "store/slices/connectionSlice";
@@ -21,6 +26,10 @@ export interface AuthHandlerContextType {
 	disconnect(): void;
 }
 
+type SessionResponse = {
+	session: DIDSession,
+	authSig: object
+};
 let session: DIDSession | null | undefined;
 
 export const AuthHandlerContext = React.createContext<AuthHandlerContextType>({} as any);
@@ -45,6 +54,58 @@ export const AuthHandlerProvider = ({ children }: any) => {
 			dispatch(setAuthLoading(false));
 		}
 	};
+
+	const startNewSession = async (): Promise<SessionResponse> => {
+		if (window.ethereum === null || window.ethereum === undefined) {
+			dispatch(setAuthLoading(false));
+			throw new Error("No injected Ethereum provider found.");
+		}
+		// We enable the ethereum provider to get the user's addresses.
+		const ethProvider = window.ethereum;
+		// request ethereum accounts.
+		const addresses = await ethProvider.enable({
+			method: "eth_requestAccounts",
+		});
+
+		const accountId = await getAccountId(ethProvider, addresses[0]);
+		const normAccount = normalizeAccountId(accountId);
+
+		const keySeed = randomBytes(32);
+		const didKey = await createDIDKey(keySeed);
+
+		const now = new Date();
+		const threeMonthsLater = new Date(now.getTime() + 90 * 24 * 60 * 60 * 1000);
+
+		const siweMessage = new SiweMessage({
+			domain: "index.as",
+			address: getAddress(normAccount.address),
+			statement: "Give this application access to some of your data on Ceramic",
+			uri: didKey.id,
+			version: "1",
+			chainId: normAccount.chainId.reference,
+			nonce: randomString(10),
+			issuedAt: now.toISOString(),
+			expirationTime: threeMonthsLater.toISOString(),
+			resources: ["ceramic://*"],
+		});
+		const signature = await ethProvider.request({
+			method: "personal_sign",
+			params: [siweMessage.signMessage(), getAddress(accountId.address)],
+		});
+		siweMessage.signature = signature;
+		const cacao = Cacao.fromSiweMessage(siweMessage);
+		const did = await createDIDCacao(didKey, cacao);
+		const session = new DIDSession({ cacao, keySeed, did });
+		return {
+			session,
+			authSig: {
+				signedMessage: siweMessage.toMessage(),
+				address: getAddress(accountId.address),
+				derivedVia: "web3.eth.personal.sign",
+				sig: siweMessage.signature,
+			},
+		} as SessionResponse;
+	};
 	const connectMetamask = async () => {
 		// Metamask Login
 		dispatch(setAuthLoading(true));
@@ -53,22 +114,11 @@ export const AuthHandlerProvider = ({ children }: any) => {
 
 		if (!connection.metaMaskConnected) {
 			if (!session || (session.hasSession && session.isExpired)) {
-				if (window.ethereum === null || window.ethereum === undefined) {
-					dispatch(setAuthLoading(false));
-					throw new Error("No injected Ethereum provider found.");
-				}
-				// We enable the ethereum provider to get the user's addresses.
-				const ethProvider = window.ethereum;
-				// request ethereum accounts.
-				const addresses = await ethProvider.enable({
-					method: "eth_requestAccounts",
-				});
-				const accountId = await getAccountId(ethProvider, addresses[0]);
-				const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId);
+				const sessionResponse = await startNewSession();
+				session = sessionResponse.session;
 				try {
-					session = await DIDSession.authorize(authMethod, { resources: ["ceramic://*"] });
-
-					localStorage.setItem("did", session.serialize());
+					localStorage.setItem("did", sessionResponse.session.serialize());
+					localStorage.setItem("authSig", JSON.stringify(sessionResponse.authSig));
 				} catch (err) {
 					console.log(err);
 				}
