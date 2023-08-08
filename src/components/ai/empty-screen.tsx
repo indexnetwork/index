787de84f846ba27886db2f1f9efafd6284130cf1
serafettin <mail@serafettin.net>
2023-08-08 13:30:31 +0300

    Update empty-screen.tsx

--- src/components/ai/empty-screen.tsx
+++ src/components/ai/empty-screen.tsx
@@ -3,47 +3,48 @@ import { UseChatHelpers } from "ai/react";
 import Button from "components/base/Button";
 
 import { IconArrowRight } from "components/ai/ui/icons";
+import Text from "../base/Text";
 
 const exampleMessages = [
 	{
-		heading: "Explain technical concepts",
-		message: `What is a "serverless function"?`,
+		heading: "What's new today?",
+		message: `Summarize today's content.`,
 	},
 	{
-		heading: "Summarize an article",
-		message: "Summarize the following article for a 2nd grader: \n",
+		heading: "Summarize my indexes",
+		message: "Summarize all my sources",
 	},
 	{
 		heading: "Draft an email",
-		message: `Draft an email to my boss about the following: \n`,
+		message: `Draft an email to my boss.`,
 	},
 ];
 
 export function EmptyScreen({ setInput }: Pick<UseChatHelpers, "setInput">) {
 	return (
-		<div className="mx-auto max-w-2xl px-4">
+		<div className="card-item mx-auto ">
 			<div className="rounded-lg border bg-background p-8">
-				<h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-				</h1>
+
+				<Text fontFamily="freizeit" size={"lg"} fontWeight={700}>
+					Chat with your indexes
+				</Text>
 				<p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chat
+					Your responses will align with all your indexes.
 				</p>
 				<p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
 				</p>
-				<div className="mt-4 flex flex-col items-start space-y-2">
-					{exampleMessages.map((message, index) => (
+				<div className="mt-4">
+					{exampleMessages.map((message, index) => (<div>
 						<Button
 							key={index}
-							variant="link"
-							className="h-auto p-0 text-base"
+							theme={"ghost"}
+							addOnBefore={<IconArrowRight width={20} />}
 							onClick={() => setInput(message.message)}
 						>
-							<IconArrowRight className="mr-2 text-muted-foreground" />
-							{message.heading}
+							<div className={"ml-2"}>{message.heading}</div>
 						</Button>
-					))}
+					</div>))}
 				</div>
 			</div>
 		</div>
