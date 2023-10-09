1455e59208b45334ca4f94efc7a2fae92fce3d45
serafettin <mail@serafettin.net>
2023-10-09 14:56:24 +0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
 	static async getInitialProps(
-		ctx: DocumentContext
+		ctx: DocumentContext,
 	): Promise<DocumentInitialProps> {
 		const sheet = new ServerStyleSheet();
 		const originalRenderPage = ctx.renderPage;
