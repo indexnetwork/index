f79c41db6310baa13973bef8562bea88a5cbd097
serafettin <mail@serafettin.net>
2023-10-14 20:24:33 +0300

    Update index.tsx

--- src/pages/[did]/index.tsx
+++ src/pages/[did]/index.tsx
@@ -34,7 +34,7 @@ const IndexesPage: NextPageWithLayout = () => {
 	useEffect(() => {
 		setChatId(uuidv4());
 		did && getProfile(did.toString());
-	}, []);
+	}, [did]);
 
 	return <PageContainer page={"profile"}>
 		<div className={"scrollable-container"}>
