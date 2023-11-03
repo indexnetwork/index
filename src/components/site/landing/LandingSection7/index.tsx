1113d7f07ec8c1237478c82c4d8403551a431ebb
Ayberk Uman <ayberkuman1@gmail.com>
2023-11-03 10:09:20 +0300

    cors error

--- src/components/site/landing/LandingSection7/index.tsx
+++ src/components/site/landing/LandingSection7/index.tsx
@@ -2,12 +2,31 @@ import Button from "components/base/Button";
 import Header from "components/base/Header";
 import Input from "components/base/Input";
 import Flex from "components/layout/base/Grid/Flex";
+import { useState } from "react";
+import externalApi from "services/external-api-service";
+import toast from "react-hot-toast";
 import LandingSection from "../LandingSection";
 
 const LandingSection7 = () => {
-  const handleSubscribe = () => {
-    /*  //TODO: handle subscribe logic */
+  const [email, setEmail] = useState("");
+
+  const handleSubscribe = async () => {
+    if (email) {
+      toast.error("Please enter a valid email address");
+    }
+    try {
+      await toast.promise(externalApi.subscribeToNewsletter(email), {
+        loading: "Subscribing...",
+        success: "Subscribed!",
+        error: "Something went wrong",
+      });
+    } catch (error) {
+      toast.error(JSON.stringify({ error }));
+    } finally {
+      setEmail("");
+    }
   };
+
   return (
     <LandingSection>
       <div>
@@ -15,7 +34,12 @@ const LandingSection7 = () => {
           Stay up to date with our newsletter
         </Header>
         <Flex className="lnd-7-form mt-8">
-          <Input inputSize="lg" placeholder="Email" />
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            inputSize="lg"
+            placeholder="Email"
+          />
           <Button onClick={handleSubscribe} size="lg">
             Subscribe
           </Button>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,24 +1,29 @@
 export const BREAKPOINTS = {
-	xs: 480,
-	sm: 768,
-	md: 992,
-	lg: 1200,
-	xl: 1201,
+  xs: 480,
+  sm: 768,
+  md: 992,
+  lg: 1200,
+  xl: 1201,
 };
 
 export const API_ENDPOINTS = {
-	CHAT_STREAM: "/chat_stream",
-	INDEXES: "/indexes",
-	SEARCH_DID: "/search/did",
-	SEARCH_LINKS: "/search/links",
-	GET_USER_INDEXES: "/search/user_indexes",
-	LIT_ACTIONS: "/lit_actions",
-	CRAWL: "/crawl/metadata",
-	CRAWL_CONTENT: "/links/crawl-content",
-	FIND_CONTENT: "/links/find-content",
-	SYNC_CONTENT: "/links/sync-content",
-	NFT_METADATA: "/nft",
-	ENS: "/ens",
-	UPLOAD_AVATAR: "/upload_avatar",
-	ZAPIER_TEST_LOGIN: "/zapier/test_login",
+  CHAT_STREAM: "/chat_stream",
+  INDEXES: "/indexes",
+  SEARCH_DID: "/search/did",
+  SEARCH_LINKS: "/search/links",
+  GET_USER_INDEXES: "/search/user_indexes",
+  LIT_ACTIONS: "/lit_actions",
+  CRAWL: "/crawl/metadata",
+  CRAWL_CONTENT: "/links/crawl-content",
+  FIND_CONTENT: "/links/find-content",
+  SYNC_CONTENT: "/links/sync-content",
+  NFT_METADATA: "/nft",
+  ENS: "/ens",
+  UPLOAD_AVATAR: "/upload_avatar",
+  ZAPIER_TEST_LOGIN: "/zapier/test_login",
+};
+
+export const EXTERNAL_API_ENDPOINTS = {
+  MAILCHIMP_SUBSCRIBE:
+    "https://index.us8.list-manage.com/subscribe/post-json?u=3e6e05095f010c187c0ee4b37&amp;id=e0b00feb51&amp;f_id=006e79e0f0",
 };
