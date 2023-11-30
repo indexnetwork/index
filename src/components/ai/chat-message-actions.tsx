1130464ec96e904ec7d4088bf5260bf8d7050a85
Ayberk Uman <ayberk@Ayberks-MacBook-Pro.local>
2023-11-30 18:00:08 +0300

    edit message functionality

--- src/components/ai/chat-message-actions.tsx
+++ src/components/ai/chat-message-actions.tsx
@@ -1,38 +1,51 @@
-import { type Message } from "ai";
-
 import Button from "components/base/Button";
 import { IconCheck, IconCopy, IconEdit } from "components/ai/ui/icons";
 
 import { useCopyToClipboard } from "hooks/useCopyToClipboard";
+import { Message } from "ai";
+import Flex from "components/layout/base/Grid/Flex";
 
 interface ChatMessageActionsProps extends React.ComponentProps<"div"> {
-  message: Message
+  message: Message;
+  handleEditClick: (message: Message, index: number) => void;
+  index: number;
+  editingMessage: Message | undefined;
 }
 
 export function ChatMessageActions({
-	message,
-	className,
-	...props
+  message,
+  handleEditClick,
+  index,
+  editingMessage,
+  className,
+  ...props
 }: ChatMessageActionsProps) {
-	const { isCopied, copyToClipboard } = useCopyToClipboard({ timeout: 2000 });
+  const { isCopied, copyToClipboard } = useCopyToClipboard({ timeout: 2000 });
 
-	const onCopy = () => {
-		if (isCopied) return;
-		copyToClipboard(message.content);
-	};
-
-	return (
-		<div
-			{...props}
-		>
-			<Button onClick={() => console.log(message.id)} theme="clear">
-				<IconEdit />
-				<span className="sr-only hidden">Edit message</span>
-			</Button>
-			<Button iconButton theme={"ghost"} onClick={onCopy}>
-				{isCopied ? <IconCheck /> : <IconCopy />}
-				<span className="sr-only hidden">Copy message</span>
-			</Button>
-		</div>
-	);
+  const onCopy = () => {
+    if (isCopied) return;
+    copyToClipboard(message.content);
+  };
+  return (
+    <div {...props}>
+      <Flex>
+        {message.role === "user" && !editingMessage?.id && (
+          <Button
+            iconButton
+            onClick={() => handleEditClick(message, index)}
+            theme="ghost"
+          >
+            <IconEdit />
+            <span className="sr-only hidden">Edit message</span>
+          </Button>
+        )}
+        {!editingMessage?.id && (
+          <Button iconButton theme={"ghost"} onClick={onCopy}>
+            {isCopied ? <IconCheck /> : <IconCopy />}
+            <span className="sr-only hidden">Copy message</span>
+          </Button>
+        )}
+      </Flex>
+    </div>
+  );
 }
