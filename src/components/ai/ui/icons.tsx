4544118eddac2faafe04d3c17d27128d1dac4193
Ayberk Uman <ayberkuman1@gmail.com>
2023-12-06 13:49:02 +0300

    icon viewbox

--- src/components/ai/ui/icons.tsx
+++ src/components/ai/ui/icons.tsx
@@ -371,7 +371,7 @@ function IconCheck({ className, ...props }: React.ComponentProps<"svg">) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="-2 -2 24 24"
       fill="none"
       {...props}
     >
@@ -404,8 +404,7 @@ function IconClose({ className, ...props }: React.ComponentProps<"svg">) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={20}
-      height={20}
+      viewBox="-2 -2 24 24"
       fill="none"
       {...props}
     >
