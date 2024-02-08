114d9c9a7ba2120672ac7f2018bf51327de33681
serafettin <mail@serafettin.net>
2024-02-08 18:32:54 +0100

    Update lit-service.ts

--- web-app/src/services/lit-service.ts
+++ web-app/src/services/lit-service.ts
@@ -7,6 +7,7 @@ import { Cacao } from "@didtools/cacao";
 import { getResolver } from "key-did-resolver";
 import { createDIDCacao, DIDSession } from "did-session";
 import { Ed25519Provider } from "key-did-provider-ed25519";
+import { CID } from "multiformats/cid";
 import { appConfig } from "../config";
 
 const checkAndSignAuthMessage = async () =>
@@ -17,40 +18,49 @@ class LitService {
     const litContracts = new LitContracts();
     await litContracts.connect();
 
+    const signerFunctionV0 = CID.parse(appConfig.defaultCID).toV0().toString();
+    const acid = litContracts.utils.getBytesFromMultihash(signerFunctionV0);
+
     const mintCost = await litContracts.pkpNftContract.read.mintCost();
-
-    const mint = await litContracts.pkpNftContract.write.mintNext(2, { value: mintCost });
-
-    const wait = await mint.wait();
-    /*
     const mint = (await litContracts.pkpHelperContract.write.mintNextAndAddAuthMethods(
         2,
         [2],
         [acid],
         ["0x"],
-        [[BigInt(0)]],
+        [[BigInt(1)]],
         false,
         false,
         {
           value: mintCost,
         },
       )) as any;
-    */
-    const pkpMintedEventTopic = ethers.id("PKPMinted(uint256,bytes)");
-		const eventLog = wait.logs.find(
-			(log: { topics: string[]; }) => log.topics[0] === pkpMintedEventTopic,
-		);
-		if (!eventLog) {
-			throw new Error("PKP minted event not found");
-		}
-			const tokenIdFromEvent = eventLog.topics[1];
-		const tokenIdNumber = BigInt(tokenIdFromEvent).toString();
-		const pkpPublicKey = await litContracts.pkpNftContract.read.getPubkey(tokenIdFromEvent);
-		console.log(
-			`superlog, PKP public key is ${pkpPublicKey} and Token ID is ${tokenIdFromEvent} and Token ID number is ${tokenIdNumber}`,
-		);
-		const acid = litContracts.utils.getBytesFromMultihash(appConfig.defaultCID);
-		const addPermissionTx = await litContracts.pkpPermissionsContract.write.addPermittedAction(tokenIdNumber, acid, []);
+    const wait = await mint.wait();
+
+    /* eslint-disable */
+    const tokenIdFromEvent = wait?.logs
+      ? wait.logs[0].topics[1]
+      : wait?.logs[0].topics[1];
+    const tokenIdNumber = BigInt(tokenIdFromEvent).toString();
+    const pkpPublicKey =
+    await litContracts.pkpNftContract.read.getPubkey(tokenIdFromEvent);
+
+    /*
+    const authMethods = await litContracts.pkpPermissionsContract.read.getPermittedAuthMethods(tokenIdFromEvent );
+
+    console.log("lit.authMethods", authMethods[0])
+    const scopes = await litContracts.pkpPermissionsContract.read.getPermittedAuthMethodScopes(
+        tokenIdFromEvent,
+        authMethods[0].authMethodType,
+        authMethods[0].id,
+        3
+    );
+    console.log("lit.scopes", scopes)
+     */
+
+    console.log(
+      `superlog, PKP public key is ${pkpPublicKey} and Token ID is ${tokenIdFromEvent} and Token ID number is ${tokenIdNumber}`,
+    );
+
     return {
       tokenIdFromEvent,
       tokenIdNumber,
@@ -58,40 +68,14 @@ class LitService {
     };
   }
 
-  async hasOriginNFT() {
-    return true;
-    if (localStorage.getItem("hasOrigin")) {
-      return true;
-    }
-    const litNodeClient = new LitNodeClient({
-      litNetwork: "cayenne",
-    });
-    await litNodeClient.connect();
-    const authSig = await checkAndSignAuthMessage();
-    const resp = await litNodeClient.executeJs({
-      ipfsId: "QmaHRJzgezMwroP1EPdWhEhSbwj3ntCk46ikAuE5jvAsX1",
-      targetNodeRange: 1,
-      authSig,
-      jsParams: {
-        authSig,
-        chain: "ethereum",
-      },
-    });
-    // @ts-ignore
-    const { hasOrigin } = resp.response;
-    if (hasOrigin) {
-      localStorage.setItem("hasOrigin", "1");
-    }
-    return hasOrigin;
-  }
 
   async getPKPSession(pkpPublicKey: string, collabAction: string) {
     const existingSessionStr = localStorage.getItem(`pkp_${pkpPublicKey}`);
     if (existingSessionStr) {
       const es = JSON.parse(existingSessionStr);
       if (
-        es.isPermittedAddress
-        || (es.isCreator && es.collabAction === collabAction)
+        es.isPermittedAddress ||
+        (es.isCreator && es.collabAction === collabAction)
       ) {
         const existing = await DIDSession.fromSession(es.session);
         await existing.did.authenticate();
