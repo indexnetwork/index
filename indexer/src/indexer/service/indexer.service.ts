a7c6508ca5b2b9ababd04d088c1d42e2e7564d10
karahan.sahin <karahan.sahin@boun.edu.tr>
2024-02-12 16:23:18 +0300

    hotfix (crawl): fix user agent header

--- indexer/src/indexer/service/indexer.service.ts
+++ indexer/src/indexer/service/indexer.service.ts
@@ -33,6 +33,9 @@ export class IndexerService {
             url: url,
             method: 'GET',
             responseType: 'stream',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+            }
         });
 
         const content_type = response.headers['content-type'].split(';')[0];
