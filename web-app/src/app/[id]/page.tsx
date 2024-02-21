78b4d6cce818a7dffbff9f39ba813198b98160e8
serhat <confxsd@gmail.com>
2024-02-21 20:17:36 +0300

    fix

--- web-app/src/app/[id]/page.tsx
+++ web-app/src/app/[id]/page.tsx
@@ -8,7 +8,7 @@ import IndexConversationSection from "components/sections/IndexConversation";
 import UserConversationSection from "components/sections/UserConversation";
 import { useRouteParams } from "hooks/useRouteParams";
 import { useEffect } from "react";
-import "../../styles/main.scss";
+// import "../../styles/main.scss";
 
 const Discovery = () => {
   const { discoveryType, indexes } = useApp();
