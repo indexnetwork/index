7fbb3534a499b088eed1ea477642a7992adc295f
serhat <confxsd@gmail.com>
2024-03-18 17:40:32 +0300

    fix demo

--- sdk-demo/index.js
+++ sdk-demo/index.js
@@ -1,46 +1,45 @@
-// import IndexClient from "./index-client";
-import IndexClient from "index-client";
+import IndexClient from "@indexnetwork/sdk";
 
 const session = "";
-const did = "";
+const did = "did:pkh:eip155:1:0x37CFc48C7f6Ff771f05a442F16674ab2757d0ed5";
 
-const signerPublicKey = "";
-const signerFunction = "";
+const privateKey = "";
 
 async function main() {
   try {
-    const indexClient = new IndexClient();
-
-    indexClient.authenticate({
-      domain: "localhost:3000",
-      session,
+    const indexClient = new IndexClient({
+      domain: "index.network",
+      network: "ethereum",
+      privateKey,
     });
+
+    indexClient.authenticate();
 
     // const indexes = await indexClient.getAllIndexes(did);
     // console.log(indexes);
-    //
-    const profile = await indexClient.getProfile(did);
-    console.log("Profile:", profile);
+
+    // const profile = await indexClient.getProfile(did);
+    // console.log("Profile:", profile);
 
     // Create a new index
-    const title = "New Index Title";
-    const newIndex = await indexClient.createIndex(
-      title,
-      signerPublicKey,
-      signerFunction,
-    );
-    console.log("New Index:", newIndex);
+    try {
+      const title = "New Index Title";
+      const newIndex = await indexClient.createIndex(title);
+      console.log("New Index:", newIndex);
 
-    const itemId = await indexClient.crawlLink(
-      "https://www.paulgraham.com/articles.html",
-    );
-    console.log("Crawled Item:", itemId);
+      // const itemId = await indexClient.crawlLink(
+      //   "https://www.paulgraham.com/articles.html",
+      // );
+      // console.log("Crawled Item:", itemId);
 
-    const addedItem = await indexClient.addItemToIndex(newIndex.id, itemId);
-    console.log("Added Item to Index:", addedItem);
+      // const addedItem = await indexClient.addItemToIndex(newIndex.id, itemId);
+      // console.log("Added Item to Index:", addedItem);
 
-    const fetchedIndex = await indexClient.getIndex(newIndex.id);
-    console.log("Fetched Index:", fetchedIndex);
+      // const fetchedIndex = await indexClient.getIndex(newIndex.id);
+      // console.log("Fetched Index:", fetchedIndex);
+    } catch (err) {
+      console.error("Error creating index:", err);
+    }
   } catch (error) {
     console.error("Error:", error);
   }
