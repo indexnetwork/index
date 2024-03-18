46fa33271fd010befda8317322e034ebd899c82f
serhat <confxsd@gmail.com>
2024-03-18 17:25:03 +0300

    sdk cleanup

---
+++ sdk/src/config.ts
@@ -0,0 +1,13 @@
+const IndexConfig = {
+  defaultCID: "Qma1fQKSYtcGZ2vJ7CHEY9B4gd967V3cX5Bfmfwp19FjDP",
+  litNetwork: "habanero" as
+    | "cayenne"
+    | "custom"
+    | "localhost"
+    | "manzano"
+    | "habanero",
+  apiURL: "https://dev.index.network/api",
+  litProtocolRPCProviderURL: "https://chain-rpc.litprotocol.com/http",
+};
+
+export default IndexConfig;
