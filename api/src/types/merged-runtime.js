5da9891c1212e6a8ad2fbf2b26389a3521fb9e7d
serafettin <mail@serafettin.net>
2024-04-15 09:00:40 -0400

    Update merged-runtime.js

--- api/src/types/merged-runtime.js
+++ api/src/types/merged-runtime.js
@@ -31,6 +31,12 @@ export const definition = {
             "id": "kjzl6hvfrbw6cb3ih99fy9ljt2ttycr213yyyxtu31fuoqbo1j78hj5zghz98v7",
             "accountRelation": {"type": "single"}
         },
+        "Team": {
+            "interface": false,
+            "implements": [],
+            "id": "kjzl6hvfrbw6cb9d3i74xp3iuooxhw04k3pumj6zyzxlvkvzyd6qaus3jihej7h",
+            "accountRelation": {"type": "list"}
+        },
         "WebPage": {
             "interface": false,
             "implements": [],
@@ -143,6 +149,169 @@ export const definition = {
             "updatedAt": {"type": "datetime", "required": true, "immutable": false},
             "controllerDID": {"type": "view", "viewType": "documentAccount"}
         },
+        "Team": {
+            "logo": {"type": "uri", "required": true, "immutable": false},
+            "name": {"type": "string", "required": true, "immutable": false},
+            "teamId": {"type": "string", "required": true, "immutable": false},
+            "members": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamMember",
+                    "required": false,
+                    "immutable": false
+                }
+            },
+            "twitter": {"type": "string", "required": false, "immutable": false},
+            "website": {"type": "uri", "required": true, "immutable": false},
+            "fundingStage": {"type": "string", "required": false, "immutable": false},
+            "industryTags": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamIndustryTag",
+                    "required": false,
+                    "immutable": false
+                }
+            },
+            "technologies": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamTechnology",
+                    "required": false,
+                    "immutable": false
+                }
+            },
+            "contactMethod": {"type": "uri", "required": true, "immutable": false},
+            "linkedinHandle": {"type": "uri", "required": true, "immutable": false},
+            "longDescription": {"type": "string", "required": false, "immutable": false},
+            "shortDescription": {"type": "string", "required": false, "immutable": false},
+            "membershipSources": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamMembershipSource",
+                    "required": false,
+                    "immutable": false
+                }
+            }
+        },
+        "TeamIndustryTag": {
+            "uid": {"type": "string", "required": true, "immutable": false},
+            "title": {"type": "string", "required": true, "immutable": false},
+            "createdAt": {"type": "datetime", "required": true, "immutable": false},
+            "updatedAt": {"type": "datetime", "required": true, "immutable": false},
+            "definition": {"type": "string", "required": false, "immutable": false},
+            "airtableRecId": {"type": "string", "required": false, "immutable": false},
+            "industryCategoryUid": {"type": "string", "required": false, "immutable": false}
+        },
+        "TeamMember": {
+            "name": {"type": "string", "required": true, "immutable": false},
+            "image": {"type": "uri", "required": true, "immutable": false},
+            "teams": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamRole",
+                    "required": false,
+                    "immutable": false
+                }
+            },
+            "skills": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamSkill",
+                    "required": false,
+                    "immutable": false
+                }
+            },
+            "twitter": {"type": "string", "required": false, "immutable": false},
+            "location": {"type": "string", "required": false, "immutable": false},
+            "mainTeam": {
+                "type": "reference",
+                "refType": "object",
+                "refName": "TeamRole",
+                "required": true,
+                "immutable": false
+            },
+            "memberId": {"type": "string", "required": true, "immutable": false},
+            "teamLead": {"type": "boolean", "required": true, "immutable": false},
+            "openToWork": {"type": "boolean", "required": true, "immutable": false},
+            "officeHours": {"type": "string", "required": false, "immutable": false},
+            "preferences": {"type": "string", "required": false, "immutable": false},
+            "githubHandle": {"type": "string", "required": false, "immutable": false},
+            "repositories": {
+                "type": "list",
+                "required": false,
+                "immutable": false,
+                "item": {"type": "string", "required": false, "immutable": false}
+            },
+            "discordHandle": {"type": "string", "required": false, "immutable": false},
+            "linkedinHandle": {"type": "uri", "required": true, "immutable": false},
+            "telegramHandle": {"type": "string", "required": false, "immutable": false},
+            "projectContributions": {
+                "type": "list",
+                "required": true,
+                "immutable": false,
+                "item": {
+                    "type": "reference",
+                    "refType": "object",
+                    "refName": "TeamProjectContribution",
+                    "required": true,
+                    "immutable": false
+                }
+            }
+        },
+        "TeamMembershipSource": {
+            "uid": {"type": "string", "required": true, "immutable": false},
+            "title": {"type": "string", "required": true, "immutable": false},
+            "createdAt": {"type": "datetime", "required": true, "immutable": false},
+            "updatedAt": {"type": "datetime", "required": true, "immutable": false}
+        },
+        "TeamProjectContribution": {
+            "uid": {"type": "string", "required": true, "immutable": false},
+            "role": {"type": "string", "required": true, "immutable": false},
+            "endDate": {"type": "datetime", "required": false, "immutable": false},
+            "memberUid": {"type": "string", "required": true, "immutable": false},
+            "startDate": {"type": "datetime", "required": false, "immutable": false},
+            "projectUid": {"type": "string", "required": true, "immutable": false},
+            "description": {"type": "string", "required": false, "immutable": false},
+            "currentProject": {"type": "boolean", "required": true, "immutable": false}
+        },
+        "TeamRole": {
+            "uid": {"type": "string", "required": true, "immutable": false},
+            "name": {"type": "string", "required": true, "immutable": false},
+            "role": {"type": "string", "required": false, "immutable": false},
+            "mainTeam": {"type": "boolean", "required": true, "immutable": false},
+            "teamLead": {"type": "boolean", "required": true, "immutable": false}
+        },
+        "TeamSkill": {"title": {"type": "string", "required": true, "immutable": false}},
+        "TeamTechnology": {
+            "uid": {"type": "string", "required": true, "immutable": false},
+            "title": {"type": "string", "required": true, "immutable": false},
+            "createdAt": {"type": "datetime", "required": true, "immutable": false},
+            "updatedAt": {"type": "datetime", "required": true, "immutable": false}
+        },
         "WebPage": {
             "url": {"type": "string", "required": true, "immutable": false},
             "title": {"type": "string", "required": false, "immutable": false},
@@ -161,6 +330,7 @@ export const definition = {
         "indexItemList": {"type": "connection", "name": "IndexItem"},
         "indexList": {"type": "connection", "name": "Index"},
         "profile": {"type": "node", "name": "Profile"},
+        "teamList": {"type": "connection", "name": "Team"},
         "webPageList": {"type": "connection", "name": "WebPage"}
     }
 }
