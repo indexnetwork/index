4fbe13710c1a899efd526a6d39ac87ecfc479f2f
serhat <confxsd@gmail.com>
2024-06-13 15:20:36 +0300

    wip

--- web-app/src/components/sections/AppRight.tsx
+++ web-app/src/components/sections/AppRight.tsx
@@ -1,18 +1,21 @@
+import { Tabs } from "@/components/base/Tabs";
+import TabPane from "@/components/base/Tabs/TabPane";
+import { useApp } from "@/context/AppContext";
 import cc from "classcat";
 import Button from "components/base/Button";
 import IconClose from "components/base/Icon/IconClose";
-import { Tabs } from "components/base/Tabs";
-import TabPane from "components/base/Tabs/TabPane";
 import Col from "components/layout/base/Grid/Col";
 import Flex from "components/layout/base/Grid/Flex";
 import FlexRow from "components/layout/base/Grid/FlexRow";
-import { useApp } from "@/context/AppContext";
 import Soon from "components/site/indexes/Soon";
 
 const AppRight = () => {
-  const {
- setRightSidebarOpen, rightSidebarOpen, rightTabKey, setRightTabKey,
-} = useApp();
+  const { setRightSidebarOpen, rightSidebarOpen, rightTabKey, setRightTabKey } =
+    useApp();
+
+  const handleRightTabChange = (tabKey: string) => {
+    setRightTabKey(tabKey);
+  };
 
   return (
     <Col
@@ -38,18 +41,101 @@ const AppRight = () => {
         flexdirection={"column"}
       >
         <FlexRow wrap={false} className={"idxflex-grow-1 mt-6"}>
-          <Tabs activeKey={"history"} onTabChange={setRightTabKey}>
-            <TabPane enabled={true} tabKey={"history"} title={`History`} />
-            <TabPane enabled={false} tabKey={"discover"} title={``} />
+          <Tabs activeKey={rightTabKey} onTabChange={handleRightTabChange}>
+            <TabPane enabled={true} tabKey={"history"} title={`History`}>
+              <div
+                className={"scrollable-area idxflex-grow-1"}
+                style={{
+                  display: "flex",
+                }}
+              >
+                {/* <div className={"ml-3"}>
+                <Soon section={"chat_history"}></Soon>
+              </div> */}
+
+                <div
+                  style={{
+                    padding: "24px 16px",
+                    display: "flex",
+                    gap: "24px",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  <div
+                    style={{
+                      borderRadius: "4px",
+                      padding: "16px",
+                      border: "1px solid var(--gray-2)",
+                      height: "fit-content",
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "16px",
+                    }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "14px",
+                        fontWeight: "bold",
+                        margin: 0,
+                      }}
+                    >
+                      Found a new AI-based Web3 protocol, Space Protocol, which
+                      simplifies data sorting on decentralized networks{" "}
+                    </p>
+
+                    <p
+                      style={{
+                        margin: 0,
+                      }}
+                    >
+                      Last message 10 days ago
+                    </p>
+                  </div>
+                  <div
+                    style={{
+                      borderRadius: "4px",
+                      padding: "16px",
+                      border: "1px solid var(--gray-2)",
+                      height: "fit-content",
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "16px",
+                    }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "14px",
+                        fontWeight: "bold",
+                        margin: 0,
+                      }}
+                    >
+                      Found a new AI-based Web3 protocol, Space Protocol, which
+                      simplifies data sorting on decentralized networks{" "}
+                    </p>
+
+                    <p
+                      style={{
+                        margin: 0,
+                      }}
+                    >
+                      Last message 10 days ago
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </TabPane>
+            <TabPane enabled={true} tabKey={"discover"} title={`Discovery`}>
+              <div
+                style={{
+                  marginTop: "48px",
+                }}
+              >
+                <Soon section={"chat_history"}></Soon>
+              </div>
+            </TabPane>
           </Tabs>
         </FlexRow>
-        {rightTabKey === "history" && (
-          <Flex className={"scrollable-area idxflex-grow-1 px-5"}>
-            <div className={"ml-3"}>
-              <Soon section={"chat_history"}></Soon>
-            </div>
-          </Flex>
-        )}
       </Flex>
     </Col>
   );
