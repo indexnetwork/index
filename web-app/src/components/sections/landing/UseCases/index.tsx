d51f899d929a122620b9c8aae0628ef3dd11cd7d
serhat <confxsd@gmail.com>
2024-06-27 15:22:12 +0300

    scroll mobile fix

--- web-app/src/components/sections/landing/UseCases/index.tsx
+++ web-app/src/components/sections/landing/UseCases/index.tsx
@@ -5,6 +5,29 @@ const UseCasesSection = () => {
   const containerRef = useRef(null);
   const [currentBlock, setCurrentBlock] = useState("block1");
 
+  // add sticky class to the container when the user scrolls to the top of the container
+  useEffect(() => {
+    const handleScroll = () => {
+      const container: any = containerRef.current;
+      if (!container) return;
+
+      const containerBoundingRect = container.getBoundingClientRect();
+      const rightContainer = document.getElementById("UseCasesRight");
+      console.log("containerBoundingRect.top", containerBoundingRect.top);
+      if (containerBoundingRect.top <= 0) {
+        rightContainer?.classList.add("sticky");
+      } else {
+        rightContainer?.classList.remove("sticky");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   useEffect(() => {
     const observerOptions = {
       root: null,
@@ -73,14 +96,14 @@ const UseCasesSection = () => {
   return (
     <section className="relative">
       <div
-        className="sticky px-8 top-0 container  m-auto h-[300vh] flex flex-row"
+        className="pt-12 md:px-8 md:container m-auto h-[358dvh] md:h-[300dvh] md:flex  md:flex-row"
         ref={containerRef}
       >
-        <div className="w-1/2 flex flex-col" id="UseCasesLeft">
+        <div className="px-4 w-full md:w-1/2 flex flex-col" id="UseCasesLeft">
           <h1 className="font-secondary text-highlightBlue text-4xl md:text-5xl font-bold">
             Index in Action
           </h1>
-          <div id="block1" className="h-[75vh] flex flex-row items-center">
+          <div id="block1" className="h-[75dvh] flex flex-row items-center">
             <div className="flex flex-col gap-4">
               <h2 className="text-3xl font-secondary font-bold">
                 Discovery for <br />{" "}
@@ -88,7 +111,7 @@ const UseCasesSection = () => {
                   Knowledge & Social Graphs
                 </span>
               </h2>
-              <p className="w-3/4 font-secondary text-lg">
+              <p className="sm:w-full md:w-3/4 font-secondary text-lg">
                 Users can leverage their social connections, trust networks, and
                 intents to discover new social interactions, events, and
                 communities
@@ -96,13 +119,13 @@ const UseCasesSection = () => {
             </div>
           </div>
 
-          <div id="block2" className="h-[75vh] flex flex-row items-center">
+          <div id="block2" className="h-[75dvh] flex flex-row items-center">
             <div className="flex flex-col gap-4">
               <h2 className="text-3xl font-secondary font-bold">
                 Discovery for <br />
                 <span className="text-highlightRed">Intent Graph </span>
               </h2>
-              <p className="w-3/4 font-secondary text-lg">
+              <p className="sm:w-full md:w-3/4 font-secondary text-lg">
                 Users can share their intents directly with multiple autonomous
                 agents, enhancing user experience and enabling discoverability
                 in different contexts.
@@ -110,13 +133,13 @@ const UseCasesSection = () => {
             </div>
           </div>
 
-          <div id="block3" className="h-[75vh] flex flex-row items-center">
+          <div id="block3" className="h-[75dvh] flex flex-row items-center">
             <div className="flex flex-col gap-4">
               <h2 className="text-3xl font-secondary font-bold">
                 Discovery for <br />{" "}
                 <span className="text-highlightPurple">Autonomous Agents</span>
               </h2>
-              <p className="w-3/4 font-secondary text-lg">
+              <p className="sm:w-full md:w-3/4 font-secondary text-lg">
                 Agents can discover and collaborate with other agents, discuss
                 specific tasks, and utilize their reputations. specific tasks,
                 and utilize their reputations. <br />
@@ -130,14 +153,14 @@ const UseCasesSection = () => {
 
           <div
             id="block4"
-            className="pb-40 h-[75vh] flex flex-row items-center"
+            className="md:pb-40 h-[75dvh] flex flex-row items-center"
           >
             <div className="flex flex-col gap-4">
               <h2 className="text-3xl font-secondary font-bold">
                 Discovery for <br />
                 <span className="text-highlightYellow">Conversations</span>
               </h2>
-              <p className="w-3/4 font-secondary text-lg">
+              <p className="sm:w-full md:w-3/4 font-secondary text-lg">
                 Initiate public conversations that are contextually discoverable
                 by others, making each conversation visible within other
                 people's conversations or anywhere else it is contextually
@@ -148,10 +171,10 @@ const UseCasesSection = () => {
         </div>
         {/* Right */}
         <div
-          className="w-1/2 sticky h-screen flex items-center top-0"
+          className="bg-[#303c56] md:bg-transparent bottom-0 md:top-0 w-full md:w-1/2  h-[50dvh] md:h-screen flex items-center "
           id="UseCasesRight"
         >
-          {renderImageBlocks(currentBlock)}
+          <div className="px-4 md:px-0">{renderImageBlocks(currentBlock)}</div>
         </div>
       </div>
     </section>
