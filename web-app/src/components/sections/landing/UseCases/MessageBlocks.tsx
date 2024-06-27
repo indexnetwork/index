1e6f467e24226d27de038fe424657d94f129a735
serhat <confxsd@gmail.com>
2024-06-27 23:18:18 +0300

    fix

--- web-app/src/components/sections/landing/UseCases/MessageBlocks.tsx
+++ web-app/src/components/sections/landing/UseCases/MessageBlocks.tsx
@@ -33,6 +33,12 @@ export const renderImageBlocks = (blockID: string) => {
             src="/images/use-cases/message4.png"
             alt="Use Cases"
             width={584}
+            height={60}
+          />
+          <Image
+            src="/images/use-cases/message5.png"
+            alt="Use Cases"
+            width={584}
             height={88}
           />
         </div>
@@ -41,25 +47,25 @@ export const renderImageBlocks = (blockID: string) => {
       return (
         <div className="image-container flex flex-col gap-4">
           <Image
-            src="/images/use-cases/message5.png"
+            src="/images/use-cases/message6.png"
             alt="Use Cases"
             width={584}
             height={60}
           />
           <Image
-            src="/images/use-cases/message6.png"
+            src="/images/use-cases/message7.png"
             alt="Use Cases"
             width={584}
             height={96}
           />
           <Image
-            src="/images/use-cases/message7.png"
+            src="/images/use-cases/message8.png"
             alt="Use Cases"
             width={584}
             height={78}
           />
           <Image
-            src="/images/use-cases/message8.png"
+            src="/images/use-cases/message9.png"
             alt="Use Cases"
             width={584}
             height={237}
@@ -70,7 +76,7 @@ export const renderImageBlocks = (blockID: string) => {
       return (
         <div className="image-container flex flex-col gap-4">
           <Image
-            src="/images/use-cases/message9.png"
+            src="/images/use-cases/message10.png"
             alt="Use Cases"
             width={584}
             height={92}
