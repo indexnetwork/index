7419933d2d2b519953ea2117f6f5ae33ebe09485
serhat <confxsd@gmail.com>
2024-06-28 00:16:00 +0300

    hero revert

--- web-app/src/components/sections/landing/Hero.tsx
+++ web-app/src/components/sections/landing/Hero.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/new/Button";
 import { useAuth } from "@/context/AuthContext";
 import useTypingAnimation from "@/hooks/useTypingAnimation";
+import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useMemo } from "react";
 
@@ -24,36 +25,40 @@ const HeroSection = () => {
   }, [query.get("allowed")]);
 
   return (
-    <section
-      className="relative"
-      style={{
-        backgroundImage: "url(/images/hero-bg.png)",
-        //cover
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="m-auto flex max-w-screen-lg flex-row gap-12 h-[80dvh] items-center lg:h-[100dvh] pb-24 pt-48 md:pb-10 lg:flex-row ">
-        <div className="font-secondary z-10 flex flex-col justify-center gap-4 px-8  lg:px-0 lg:pr-6 ">
-          <h1 className="hidden md:block font-title text-[1.9rem] leading-[2.25rem] md:text-7xl md:leading-[5.2rem]">
+    <section className="relative">
+      <div className="m-auto flex flex-col gap-12 lg:h-[100dvh] pb-12 pt-12 md:pb-16 lg:flex-row lg:justify-start">
+        <div className="relative self-center flex items-center w-full pt-4 md:mt-12 lg:mt-0 md:w-1/2  max-w-screen-lg">
+          <Image
+            src="/images/hero.png"
+            alt="Hero"
+            width={716}
+            height={523}
+            layout="responsive"
+          />
+        </div>
+
+        <div className="lg:pt-32 font-secondary z-10 flex flex-col items-center justify-center gap-4 px-8 text-center  lg:w-[575px] lg:items-start lg:px-0 lg:pr-6 ">
+          {/* <h1 className="hidden md:block md:text-center lg:text-start font-title text-[1.9rem] leading-[2.25rem] md:text-5xl md:leading-[4rem]">
             The First Discovery Protocol <br /> for{" "}
             <span className="text-highlightBlue">{typingText}|</span>
-          </h1>
-          <h1 className="md:hidden font-title text-[1.9rem] leading-[2.25rem] md:text-6xl md:leading-[4.5rem]">
+          </h1> */}
+          <h1 className="font-title text-center lg:text-start text-[1.9rem] leading-[2.25rem] md:text-4xl lg:text-5xl lg:leading-[3.6rem]">
             The First <br />
             Discovery Protocol <br /> for{" "}
             <span className="text-highlightBlue">{typingText}|</span>
           </h1>
 
-          <p className="hidden md:block text-base md:text-xl">
-            Index allows you to create truly personalised and <br /> autonomous
-            discovery experiences across the web
+          <p className="hidden text-start md:block text-base md:text-xl">
+            Index allows you to create truly personalised and
+            <br />
+            autonomous discovery experiences across the web.
           </p>
           <p className="md:hidden text-base md:text-xl">
             Index allows you to create truly personalised and autonomous
-            discovery experiences across the web
+            discovery experiences across the web.
           </p>
-          <div className="flex gap-4 pt-4">
-            {allowed && <Button onClick={connect}>Connect</Button>}
+          <div className="flex gap-4">
+            <Button onClick={connect}>Connect</Button>
             <Button
               variant="outline"
               onClick={() => {
