04766f42a1d15ac6838f7c274f89b1330455a705
serhat <confxsd@gmail.com>
2024-06-27 23:00:58 +0300

    css cleanup

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -10,7 +10,7 @@ import { AuthStatus, useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { Suspense, useEffect } from "react";
 import "./globals.css";
-// import "./landing.css";
+import "./landing.css";
 import HowItWorksSection from "@/components/sections/landing/HowItWorks";
 import UseCasesSection from "@/components/sections/landing/UseCases";
 
