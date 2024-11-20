fefd89bdfff8a7bceeff5dacc9f1da551dc60ef4
serefyarar <mail@serafettin.net>
2024-11-19 23:01:17 -0500

    iterate

--- api/src/packages/api.js
+++ api/src/packages/api.js
@@ -7,8 +7,28 @@ import * as ejv from "express-joi-validation";
 
 import RedisClient from "../clients/redis.js";
 
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // Add our Profiling integration
+    nodeProfilingIntegration(),
+  ],
+
+  // Add Tracing by setting tracesSampleRate
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set sampling rate for profiling
+  // This is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
 
 const app = express();
+
+Sentry.setupExpressErrorHandler(app);
 
 
 if (process.env.NODE_ENV !== "production") {
--- api/src/packages/consumer.js
+++ api/src/packages/consumer.js
@@ -1,4 +1,15 @@
 import dotenv from "dotenv";
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
+
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
