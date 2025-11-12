54cb68368566dccde50f7280abac718a0c5ae6a3
Marcin Moldrzyk <martinxy80@protonmail.com>
2025-11-12 17:28:41 +0100

    fix dirname and filename in vitest.config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,5 +1,10 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   test: {
